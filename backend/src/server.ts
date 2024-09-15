c898f6808938109bd17dc9c8e6096a4e2eb78603
Mayderson Mello <maydersonmello@gmail.com>
2024-09-15 16:55:48 -0300

    Iniciado estrutura inicial do backend

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { resolve } from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('SERVER IS RUNNING AT :3333');
+});
